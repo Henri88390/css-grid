8bdc11142c1e092e2257e54ad20815acc52871d6
Henri88390 <henri.lambert9@gmail.com>
2023-11-01 10:25:45 +0100

    feat: add grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="grid-container">
+      <div className="grid-item grid-item-1"></div>
+      <div className="grid-item grid-item-2"></div>
+      <div className="grid-item grid-item-3"></div>
+      <div className="grid-item grid-item-3"></div>
+      <div className="grid-item grid-item-3"></div>
+      <div className="grid-item grid-item-3"></div>
+      <div className="grid-item grid-item-3"></div>
+    </div>
+  );
 };
 
 export default App;
